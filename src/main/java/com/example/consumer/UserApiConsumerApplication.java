60eb8fca75da3629a315a56fda4378e95509bc2f
saiteja-1304 <saitejapalle0506@gmail.com>
2024-07-31 09:36:10 +0530

    awsdeploying

---
+++ src/main/java/com/example/consumer/UserApiConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.example.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiConsumerApplication.class, args);
+	}
+//4c31c68e5dbccfbf4d5f6581fbaa102da4c8c4d03ae5c1ca4c6c8fc7030f9414
+	//a49ed347d16a4ea1be1d0821d12690d9
+}
